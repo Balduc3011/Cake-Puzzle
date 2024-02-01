1af18dc09b8e7e236baa642bb56e40ba443119fe
Balduu Nguyen <ducnb3011@gmail.com>
2024-02-01 23:58:21 +0700

    UI Transition

--- Assets/_Game/Scripts/UI/PanelPlayGame/PanelPlayGame.cs
+++ Assets/_Game/Scripts/UI/PanelPlayGame/PanelPlayGame.cs
@@ -11,19 +11,39 @@ public class PanelPlayGame : UIPanel
     //[SerializeField] Button item1Btn;
     //[SerializeField] Button item2Btn;
     //[SerializeField] Button item3Btn;
+    [SerializeField] List<TransitionUI> transitionUIList;
     public override void Awake()
     {
         panelType = UIPanelType.PanelPlayGame;
         base.Awake();
     }
 
-    public void OutItemMode()
-    {
-        
-    }
-
     public void UsingItemMode()
     {
-        
+        for (int i = 0; i < transitionUIList.Count; i++)
+        {
+            transitionUIList[i].OnShow(false);
+        }
+    }
+
+    public void OutItemMode()
+    {
+        for (int i = 0; i < transitionUIList.Count; i++)
+        {
+            transitionUIList[i].OnShow(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            UsingItemMode();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            OutItemMode();
+        }
     }
 }
