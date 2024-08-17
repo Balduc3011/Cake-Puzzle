9d175502ea49ec111c0196ed132dfa8e957507f9
hoa chu <hoacmgch18105@gmail.com>
2024-08-17 17:33:13 +0700

    update

--- Assets/_Game/Scripts/UI/PanelPlayGame/WrapOrder.cs
+++ Assets/_Game/Scripts/UI/PanelPlayGame/WrapOrder.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using DG.Tweening;
 using System;
 
@@ -9,20 +11,36 @@ public class WrapOrder : MonoBehaviour
     [SerializeField] List<SlotOrder> slotOrders = new();
     [SerializeField] RectTransform rectWrapSlotOrder;
     [SerializeField] Transform trsBox;
+    [SerializeField] TextMeshProUGUI txtTime;
+    [SerializeField] float timeSetting;
 
     Vector2 vectorScale = Vector2.zero;
     Vector2 vectorDefault = Vector2.zero;
     Vector3 vectorPunch = new Vector3(.5f, .5f, .5f);
 
     Sequence mySequence;
+    Sequence sequenceTime;
 
     private void Awake()
     {
         vectorDefault = rectWrapSlotOrder.sizeDelta;
         RandomOrder();
     }
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            OnOrderComplete(false);
+        }
+    }
     public void RandomOrder() {
+        if (sequenceTime != null) sequenceTime.Kill();
+        sequenceTime = DOTween.Sequence();
+        sequenceTime.Append(DOVirtual.Float(timeSetting, 0, timeSetting, (value) => {
+            txtTime.text = TimeUtil.TimeToString(((int)value + 1));
+        }).SetEase(Ease.Linear).OnComplete(()=> {
+            if (!IsDoneAll()) OnOrderComplete(false);
+        }));
         if (mySequence != null)
             mySequence.Kill();
         mySequence = DOTween.Sequence();
@@ -35,20 +53,24 @@ public class WrapOrder : MonoBehaviour
         }
     }
 
-    public void OnOrderComplete() {
-        ProfileManager.Instance.playerData.playerResourseSave.AddMoney(15);
+    public void OnOrderComplete(bool isDone) {
+        
 
         vectorScale = rectWrapSlotOrder.sizeDelta;
-        vectorScale.x = -Screen.width;
+        vectorScale.x = 0;
         if (mySequence != null)
             mySequence.Kill();
         mySequence = DOTween.Sequence();
         mySequence.Append(rectWrapSlotOrder.DOSizeDelta(vectorScale, .25f, true));
         mySequence.Append(trsBox.DOPunchScale(vectorPunch, .25f));
-        mySequence.OnComplete(()=> {
-            CoinEffect coinEffect = GameManager.Instance.objectPooling.GetCoinEffect();
-            coinEffect.transform.position = trsBox.position;
-            coinEffect.Move(UIManager.instance.panelTotal.GetCoinTrs());
+      
+        mySequence.OnComplete(() => {
+            if (isDone)
+            {
+                CoinEffect coinEffect = GameManager.Instance.objectPooling.GetCoinEffect();
+                coinEffect.transform.position = trsBox.position;
+                coinEffect.Move(UIManager.instance.panelTotal.GetCoinTrs());
+            }
             RandomOrder();
         });
     }
@@ -64,12 +86,19 @@ public class WrapOrder : MonoBehaviour
             }
         }
 
+        if (IsDoneAll())
+        {
+            ProfileManager.Instance.playerData.playerResourseSave.AddMoney(15);
+            OnOrderComplete(true);
+        }
+    }
+
+    bool IsDoneAll() {
         for (int i = 0; i < slotOrders.Count; i++)
         {
             if (!slotOrders[i].IsDone())
-                return;
+                return false;
         }
-
-        OnOrderComplete();
+        return true;
     }
 }
